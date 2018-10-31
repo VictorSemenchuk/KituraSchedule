8a83355aed079accc1e08459ada9a1566ebae231
Dmitriy <dima_tarelkin@mail.ru>
2018-10-31 11:57:48 +0300

    add AppDelegate startPoint

--- KituraTeamSchedule/KituraTeamSchedule/AppDelegate.swift
+++ KituraTeamSchedule/KituraTeamSchedule/AppDelegate.swift
@@ -16,10 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow.init(frame: UIScreen.main.bounds)
-        let initialVC = ViewController()
-        initialVC.title = "Tasks"
-        let navController = UINavigationController.init(rootViewController: initialVC)
-        self.window?.rootViewController = navController
+        let welcome = WelcomeViewController()
+
+        self.window?.rootViewController = welcome
         self.window?.makeKeyAndVisible()
         return true
     }
